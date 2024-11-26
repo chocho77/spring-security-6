008bb412ce6c6b2a890d01fa2cbd48017d3f90f2
Chavdar Momchilov <chavmom@gmail>
2024-11-26 22:48:59 +0200

    Changes to be committed: 	modified:   src/main/java/com/dailycodebuffer/security/controller/ProductController.java

--- src/main/java/com/dailycodebuffer/security/controller/ProductController.java
+++ src/main/java/com/dailycodebuffer/security/controller/ProductController.java
@@ -1,10 +1,32 @@
 package com.dailycodebuffer.security.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/product")
 public class ProductController {
+
+    private record Product(Integer productId, 
+                           String productName, 
+                           double price){}
+    
+    List<Product> products = new ArrayList<>(
+        List.of(
+            new Product(1, "iPhone", 999.0),
+            new Product(1, "Mac Pro", 2099.0)
+        )
+    );
+
+    @PostMapping
+    public List<Product> getProducts() {
+        return products;
+    }
+
+
 
 }
